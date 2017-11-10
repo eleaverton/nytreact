a245cae6c201ecbe963d29a3a1643c1bff0b200c
Liz Leaverton <egleaverton@gmail.com>
2017-11-09 21:08:24 -0600

    backend build, building components

---
+++ nytreact/client/src/pages/Articles/Articles.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+
+class Main extends Component{
+
+};
+
+componentDidMount(){
+
+}
+
+render(){
+	return(
+		<div style={{ height: 300 }} className="jumbotron">
+    		New York Times Article Search
+  		</div>;
+		)
+}
+
+export default Main;
